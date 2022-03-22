99531427df04570235ffd50b43751a2d715272c2
Fede <federico.pochat@ing.austral.edu.ar>
2022-03-22 14:31:38 -0300

    Edited AddCourse button down

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -7,18 +7,20 @@ import {IoIosCheckmark, IoIosCreate, IoIosTrash} from "react-icons/io";
 function AddCourse(props) {
 
     const [tempTeeBox, setTempTeeBox] = useState('');
-    const [court, setCourt] = useState('');
+    const [course, setCourse] = useState('');
     const [location, setLocation] = useState('');
 
     let [teeboxes, setTeeBoxes] = useState([]);
 
     let navigate = useNavigate();
-    const submitCourt = (e) => {
+    const submitCourse = (e) => {
         e.preventDefault();
+
+        /*
         const users = {
             'Court' : court,
             'Location' : location,
-        }
+        }*/
         let path = '/home';
         navigate(path);
     }
@@ -42,9 +44,9 @@ function AddCourse(props) {
     }
 
     return (
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center h-100 overflow-hidden">
             <link rel="stylesheet" href={require('./Login.css')}/>
-            <div className="d-flex flex-column w-100 justify-content-center">
+            <div className="d-flex flex-column w-100 justify-content-center ">
                 <form onKeyPress={event => {
                         if (event.key === 'Enter'){
                             event.preventDefault();
@@ -56,7 +58,7 @@ function AddCourse(props) {
                         <div className="form-group w-25 mx-auto">
                             <label htmlFor="exampleInputUsername1" className="form-label mt-4">Course name</label>
                             <input type="text" className="form-control" id={"court"}
-                                   placeholder="Enter course name..." onChange={e => setCourt(e.target.value)}/>
+                                   placeholder="Enter course name..." onChange={e => setCourse(e.target.value)}/>
                         </div>
 
                         <div className="row mt-5">
@@ -135,11 +137,10 @@ function AddCourse(props) {
                             </div>
                         </div>
 
-                        <button type="submit" className="btn btn-primary" onClick={submitCourt}>Add Court</button>
+                        <button type="submit" className="btn btn-primary fixed-bottom mx-auto w-25 m-2" onClick={submitCourse}>Next</button>
 
                     </fieldset>
                 </form>
-
             </div>
         </div>
     );
